74be998a796e0076c7d1a742834b3a0959bb923f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-08-18 09:21:02 +0200

    Added logging to file

--- Signal.Beacon.WorkerService/Program.cs
+++ Signal.Beacon.WorkerService/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 using Signal.Beacon.Application;
 using Signal.Beacon.Application.Signal;
 using Signal.Beacon.Channel.iRobot;
@@ -17,7 +19,19 @@ namespace Signal.Beacon
 {
     public static class Program
     {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File("Logs/log.log", rollingInterval: RollingInterval.Day,
+                    retainedFileTimeLimit: TimeSpan.FromDays(7))
+                .WriteTo.Console()
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -37,6 +51,7 @@ namespace Signal.Beacon
                         .AddVoice();
 
                     services.AddTransient(typeof(Lazy<>), typeof(Lazier<>));
-                });
+                })
+                .UseSerilog();
     }
 }
