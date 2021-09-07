1156bb5e60443b1a65a4280787dc8312b2943081
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-09-07 19:28:37 +0200

    feat(App): Removed unused mqtt server

--- Signal.Beacon.Application/ApplicationWorkerService.cs
+++ Signal.Beacon.Application/ApplicationWorkerService.cs
@@ -11,72 +11,12 @@ using Signal.Beacon.Core.Workers;
 
 namespace Signal.Beacon.Application
 {
-    internal class MqttServer
-    {
-        private IMqttServer? mqttServer;
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            const int port = 1883;
-            var optionsBuilder = new MqttServerOptionsBuilder()
-                .WithDefaultEndpoint()
-                .WithDefaultEndpointPort(port)
-                .WithConnectionValidator(
-                    c =>
-                    {
-                        // TODO: Do auth
-                        //var currentUser = config.Users.FirstOrDefault(u => u.UserName == c.Username);
-
-                        //if (currentUser == null)
-                        //{
-                        //    c.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
-                        //    return;
-                        //}
-
-                        //if (c.Username != currentUser.UserName)
-                        //{
-                        //    c.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
-                        //    return;
-                        //}
-
-                        //if (c.Password != currentUser.Password)
-                        //{
-                        //    c.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
-                        //    return;
-                        //}
-
-                        c.ReasonCode = MqttConnectReasonCode.Success;
-                    })
-                .WithSubscriptionInterceptor(c => c.AcceptSubscription = true)
-                .WithApplicationMessageInterceptor(c => c.AcceptPublish = true);
-
-            this.mqttServer = new MqttFactory().CreateMqttServer();
-            await this.mqttServer.StartAsync(optionsBuilder.Build());
-
-            cancellationToken.Register(this.OnCancelled);
-        }
-
-        private async void OnCancelled()
-        {
-            try
-            {
-                if (this.mqttServer != null) 
-                    await this.mqttServer.StopAsync();
-            }
-            catch
-            {
-                // TODO: Log error stopping server
-            }
-        }
-    }
-
     internal class ApplicationWorkerService : IWorkerService
     {
         private readonly IProcessor processor;
         private readonly ISignalSignalRDevicesHubClient devicesHubClient;
         private readonly ISignalSignalRConductsHubClient conductsHubClient;
         private readonly IConductManager conductManager;
-        private MqttServer? mqttServer;
 
         public ApplicationWorkerService(
             IProcessor processor,
@@ -96,9 +36,6 @@ namespace Signal.Beacon.Application
             _ = this.conductsHubClient.StartAsync(cancellationToken);
             await this.processor.StartAsync(cancellationToken);
             await this.conductManager.StartAsync(cancellationToken);
-
-            //this.mqttServer = new MqttServer();
-            //await this.mqttServer.StartAsync(cancellationToken);
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
