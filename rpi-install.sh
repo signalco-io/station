c69954b2700271c6d4b3bf4d569e877836860454
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-09-07 18:27:59 +0200

    Update rpi-install.sh

--- rpi-install.sh
+++ rpi-install.sh
@@ -1,11 +1,16 @@
 # Create SWAP file
-echo "Configuring SWAP file..."
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+grep -q "swapfile" /etc/fstab
+if [ $? -ne 0 ]; then
+	echo 'swapfile not found. Adding swapfile...'
+  sudo fallocate -l 1G /swapfile
+  sudo chmod 600 /swapfile
+  sudo mkswap /swapfile
+  sudo swapon /swapfile
+  sudo cp /etc/fstab /etc/fstab.bak
+  echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+else
+  echo 'swapfile already configured'
+fi
 
 ## Configure hostname
 echo "Setting hostname to 'signalcostation'..."
