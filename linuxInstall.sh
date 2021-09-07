056f6f47a8c70946bd63ec497a5c2de97b2c7ea8
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-09-07 18:08:26 +0200

    Update linuxInstall.sh

--- linuxInstall.sh
+++ linuxInstall.sh
@@ -1,35 +1,44 @@
-# TODO: Open port 1883 for mosquitto
-
-## Disable snap updates (metered connection)
-sudo snap set system refresh.metered=hold
+# Create SWAP file
+echo "Configuring SWAP file..."
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 ## Configure hostname
-echo "Setting hostname to 'signalcostation'"
+echo "Setting hostname to 'signalcostation'..."
 sudo hostnamectl set-hostname signalcostation
 
 ## Configure firewall
-echo "Configuring firewall"
+echo "Configuring firewall..."
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw allow 1883 # Allow MQTT
+sudo ufw allow 8080 # Allow Z2M UI (Optional)
+sudo ufw allow 80 # Allow Station UI
 sudo ufw enable
 
 ## Housekeeping
 echo "Updating system..."
 sudo bash -c 'for i in update {,dist-}upgrade auto{remove,clean}; do apt-get $i -y; done'
+
+### Disable snap updates (metered connection) and do update now
+echo "Disableing SNAP updates..."
+sudo snap set system refresh.metered=hold
+echo "Updating snaps..."
 sudo snap refresh
 
+## Install node
+echo "Checking if NodeJS is installed..."
 node=$(which npm)
 if [ -z "${node}" ]; then #Installing NodeJS if not already installed.
   printf "Downloading and installing NodeJS...\\n"
   curl -sL https://deb.nodesource.com/setup_14.x | bash -
   sudo apt install -y nodejs npm
 fi
-
-# echo "Adding Microsoft Ubuntu 20.10 package signing key to list of trusted keys and add the package repository..."
-# wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-# sudo dpkg -i packages-microsoft-prod.deb
 
 # Install prerequesites
 echo "Installing dependencies..."
@@ -64,27 +73,25 @@ WantedBy=multi-user.target" > $service_path
 sudo systemctl enable signalcostation.service
 
 echo "Cloning dev branch of Zigbee2mqtt git repository..."
-sudo git clone --single-branch --branch dev https://github.com/Koenkk/zigbee2mqtt.git  /opt/zigbee2mqttdev
-sudo chown -R ubuntu:ubuntu /opt/zigbee2mqttdev
+sudo git clone --single-branch --branch master https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
+sudo chown -R ubuntu:ubuntu /opt/zigbee2mqtt
 
 echo "Running zigbee2mqtt install... This might take a while and can produce som expected errors"
-cd /opt/zigbee2mqttdev || exit
+cd /opt/zigbee2mqtt || exit
 npm ci
 
-echo "Creating service file zigbee2mqttdev.service and enableing..."
-service_path="/etc/systemd/system/zigbee2mqttdev.service"
+echo "Creating service file zigbee2mqtt.service and enableing..."
+service_path="/etc/systemd/system/zigbee2mqtt.service"
 echo "[Unit]
-Description=zigbee2mqttdev
+Description=zigbee2mqtt
 After=network.target
 [Service]
 ExecStart=/usr/bin/npm start
-WorkingDirectory=/opt/zigbee2mqttdev
+WorkingDirectory=/opt/zigbee2mqtt
 StandardOutput=inherit
 StandardError=inherit
 Restart=always
 User=ubuntu
 [Install]
 WantedBy=multi-user.target" > $service_path
-sudo systemctl enable zigbee2mqttdev.service
-
-#rsync -a 
+sudo systemctl enable zigbee2mqtt.service
